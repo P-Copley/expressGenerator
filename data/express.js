b6e061db02fdd1945c0dffc7249054c0654b327d
Paul Copley <pkcopley@gmail.com>
2018-04-26 22:47:33 +0100

    Update dirnames

--- data/express.js
+++ data/express.js
@@ -4,5 +4,5 @@ console.log(process.argv);
 console.log(process.argv0);
 
 
-exports.app = fs.readFileSync(path.join(__dirname, 'templates/app.js'));
-exports.listen = fs.readFileSync(path.join(__dirname, 'templates/listen.js'));
+exports.app = fs.readFileSync(path.join(__dirname, '..', 'templates/app.js'));
+exports.listen = fs.readFileSync(path.join(__dirname, '..', 'templates/listen.js'));
