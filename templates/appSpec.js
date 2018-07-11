21f28d5b7b6ac83fc8104d4b0e135985732b7d0a
Paul Copley <pkcopley@gmail.com>
2018-07-11 15:40:14 +0100

    Add installation logs

---
+++ templates/appSpec.js
@@ -0,0 +1,12 @@
+process.env.NODE_ENV = 'test';
+const { expect } = require('chai');
+const app = require('../app');
+const request = require('supertest')(app);
+
+describe('/api', () => {
+  describe('', () => {
+    it('', () => {
+      
+    });
+  });
+});
